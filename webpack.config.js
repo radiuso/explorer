2d5d52c57e3d580414d96749ea4b32b3486094a8
radiusario <radiusario@gmail.com>
2017-09-16 15:58:19 +0200

    add icon lib

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,13 @@ module.exports = {
         }],
         fallback: "style-loader"
       })
-    }]
+    },
+    { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+    { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+    { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
+    { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file-loader" },
+    { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url-loader?limit=10000&mimetype=image/svg+xml" }
+    ]
   },
   plugins: [
     extractSass
